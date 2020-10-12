8e17f573c1a9fdcec1745f8bfe7d900801c2c4b9
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2020-10-13 03:35:34 +0600

    Defining channel model & adding type definitions

--- src/channels/channel.model.ts
+++ src/channels/channel.model.ts
@@ -1,5 +1,5 @@
 export interface Channel {
-  channel_url: string;
+  channel_url: string; //this is the id for the youtube channel
   socialblade_category: string;
   subscribers: number;
   location: string;
@@ -12,7 +12,17 @@ export interface Channel {
   tictok: string;
   pinterest: string;
   others: string;
-  joined: any;
+  joined: string;
   views: number;
-  socialblade_similar_scraped: any;
+  //this is the status of the channel if it is previously seen
+  status: SeenStatus;
 }
+
+// we will only allow two kinds of status(Locked & Unlocked)
+export enum SeenStatus {
+  LOCKED = 'LOCKED',
+  UNLOCKED = 'UNLOCKED',
+}
+
+//Interface is TypeScript concept that simply inforces the shape of an object upon compilation
+//Thus After compulation, interfaces are not preserved as interface anymore
--- src/channels/channels.service.ts
+++ src/channels/channels.service.ts
@@ -3,7 +3,8 @@ import { Channel } from './channel.model';
 
 @Injectable()
 export class ChannelsService {
-  private channels = [];
+  //this array is the property of this class
+  private channels: Channel[] = [];
 
   getAllChannels(): Channel[] {
     return this.channels;
@@ -15,3 +16,8 @@ export class ChannelsService {
 //Providers can be a plain value, a class or sync/async factory etc
 //Providers must be provided to a module for them to be usable
 //Providers can be exported from a module & then be available to other modules that import it
+
+// -------------Services----------
+//defined as providers, Not all providers are services
+//Services are singleton when wrapped with @Injectable & provided to a module. Thus, the same instabce will be shared across the application acting as a single source of truth
+//Servies are the main source of business logic  (For Example, Services will be called from a controller to validate a data, create an item in the DB and return a response)
