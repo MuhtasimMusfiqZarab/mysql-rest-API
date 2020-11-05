068f6105a998811017712a7243ed5c26cfc90652
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2020-11-05 22:24:45 +0600

    create Channel Entity

---
+++ src/channels/channel.entity.ts
@@ -0,0 +1,19 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { SeenStatus } from './channel.model';
+
+//ENTITY naming should be in uppercase
+@Entity()
+export class Channel extends BaseEntity {
+  //continue defining the property of a task
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: SeenStatus.LOCKED;
+}
