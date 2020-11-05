1d2da368a4f405aed17cff9e68df72025bde7af4
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2020-11-05 18:50:41 +0600

    connecting remote mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 //this is the root module which is nest js module
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChannelsModule } from './channels/channels.module';
 
 @Module({
   //importing all the separate modules here
-  imports: [ChannelsModule],
+  imports: [
+    ChannelsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'vmi427508.contaboserver.net',
+      port: 3306,
+      username: 'root',
+      password: 'H4DvVLcxW5paef',
+      database: 'youtube',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
 
@@ -13,7 +26,7 @@ export class AppModule {}
 
 ///---------------Property inside a module-----------------
 // providers: array of providers to be available within the module via dependency injection
- // controllers: Array of controllers to be instantiated  within the module
- //exports: array of providers to export to other moduels
- //imports: List of modules required by this modules. Any exported provider by these moduels will now be available in our module by dependency injection.
- //
+// controllers: Array of controllers to be instantiated  within the module
+//exports: array of providers to export to other moduels
+//imports: List of modules required by this modules. Any exported provider by these moduels will now be available in our module by dependency injection.
+//
