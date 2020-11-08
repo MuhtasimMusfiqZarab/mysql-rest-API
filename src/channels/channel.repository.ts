81ac45dda1f9d9e1155593de031135e4e8143068
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2020-11-08 13:25:34 +0600

    adding repository file

--- src/channels/channel.repository.ts
+++ src/channels/channel.repository.ts
@@ -1,4 +1,5 @@
 //repository patern lets us abstract the database operations seperately
+//this is a persistance layer in the shape of a repository
 
 import { Repository, EntityRepository } from 'typeorm';
 import { Channel } from './channel.entity';
