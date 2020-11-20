558e9a0626d4ae1526bef4f33ac2d8c7514a9d12
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2020-11-20 15:47:19 +0600

    signing a jwt token upon authorization

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,18 +2,23 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth.credentials.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
   }
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     //we will get null or username after name & password validation
     const username = await this.userRepository.validatePassword(
       authCredentialsDto,
@@ -22,5 +27,12 @@ export class AuthService {
     if (!username) {
       throw new UnauthorizedException('Invalid Credentials');
     }
+
+    //payload will only consist of username, not any sensitive data
+    const payload: JwtPayload = { username };
+    //generating the access token
+    const accessToken = await this.jwtService.sign(payload);
+
+    return { accessToken };
   }
 }
